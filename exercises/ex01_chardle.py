2b65c921aafa75bf9af8419258e84c73ab98d219
Connor Hall <cbhall@unc.edu>
2022-08-28 23:39:00 -0400

    finished chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,4 @@
-"""EX01 - Chardle - A Cute step toward Wordle"""
+"""EX01 - Chardle - A Cute step toward Wordle."""
 
 __author__ = "730465288"
 
@@ -15,15 +15,15 @@ if len(str(user_character)) != 1:
 print("Searching for " + str(user_character) + " in " + str(user_word))
 
 if user_character == user_word[0]:
-    print (user_character + " found at index 0")
+    print(user_character + " found at index 0")
 if user_character == user_word[1]:
-    print (user_character + " found at index 1")
+    print(user_character + " found at index 1")
 if user_character == user_word[2]:
-    print (user_character + " found at index 2")
+    print(user_character + " found at index 2")
 if user_character == user_word[3]:
-    print (user_character + " found at index 3")
+    print(user_character + " found at index 3")
 if user_character == user_word[4]:
-    print (user_character + " found at index 4")
+    print(user_character + " found at index 4")
 
 indecies: int = 0
 if user_character == user_word[0]:
