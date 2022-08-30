665e21eaefd5814440bbca337224e4c29d6eede3
Connor Hall <cbhall@unc.edu>
2022-08-30 19:00:25 -0400

    work

---
+++ lessons/CQ03.py
@@ -0,0 +1,18 @@
+"""A tracing challenge question."""
+
+
+
+i: int = 0
+s: str = ""
+
+while i < 6:
+    if i % 2 == 0:
+        s = s + "h"
+    else: 
+        if i % 3 == 0:
+            s = s + "!"
+        else:
+            s = s + "e"
+    i = i + 1
+
+print(s)
