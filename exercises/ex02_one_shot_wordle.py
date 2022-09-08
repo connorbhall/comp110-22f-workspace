bcda18fc24db35488660b89922e33053c55a49f0
Connor Hall <cbhall@unc.edu>
2022-09-08 17:26:57 -0400

    One Shot Wordle!

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,42 @@
+"""EX02 - One Step Wordle - One Step Closer to Wordle."""
+
+__author__ = "730465288"
+
+SECRET: str = "cookie"
+guess: str = len(SECRET)
+
+user_word: str = str(input(f"What is your {guess} letter guess? "))
+
+while len(user_word) != len(SECRET):
+    user_word: str = str(input(f"That was not {guess} letters. Try again: "))
+
+white_box: str = "\U00002B1C"
+yellow_box: str = "\U0001F7E8"
+green_box: str = "\U0001F7E9"
+
+wordle: str = ""
+i: int = 0
+ii: int = 0
+index: bool = False
+
+while i < len(SECRET):
+    if user_word[i] == SECRET[i]:
+        wordle += str(green_box)
+    else:
+        while bool(index) is not True and ii < 6:
+            if user_word[i] == SECRET[ii]:
+                index: bool = True
+                wordle += str(yellow_box)
+            else: ii += 1
+        if bool(index) == False:
+            wordle += str(white_box)
+        else: index: bool = False  
+        ii: int = 0     
+    i += 1
+            
+print(wordle)
+
+if str(user_word) == str(SECRET):
+    print(f"Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
