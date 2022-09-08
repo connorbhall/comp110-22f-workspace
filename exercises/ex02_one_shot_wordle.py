579e409c5781aa169fb4eb0f2a3e725a3319176d
Connor Hall <cbhall@unc.edu>
2022-09-08 17:36:15 -0400

    One Shot Worlde

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,12 +3,12 @@
 __author__ = "730465288"
 
 SECRET: str = "python"
-guess: str = len(SECRET)
+guess = str(len(SECRET))
 
 user_word: str = str(input(f"What is your {guess} letter guess? "))
 
 while len(user_word) != len(SECRET):
-    user_word: str = str(input(f"That was not {guess} letters. Try again: "))
+    user_word = str(input(f"That was not {guess} letters. Try again: "))
 
 white_box: str = "\U00002B1C"
 yellow_box: str = "\U0001F7E8"
@@ -25,13 +25,15 @@ while i < len(SECRET):
     else:
         while bool(index) is not True and ii < 6:
             if user_word[i] == SECRET[ii]:
-                index: bool = True
+                index = True
                 wordle += str(yellow_box)
-            else: ii += 1
+            else: 
+                ii += 1
         if bool(index) == False:
             wordle += str(white_box)
-        else: index: bool = False  
-        ii: int = 0     
+        else: 
+            index = False  
+        ii = 0     
     i += 1
             
 print(wordle)
